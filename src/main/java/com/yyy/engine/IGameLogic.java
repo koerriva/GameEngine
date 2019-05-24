ee41c5f7b9df8a334599f98dcbc0305155550a7e
koerriva <rivakoer@gmail.com>
2019-05-24 18:20:14 +0800

    chapter02

---
+++ src/main/java/com/yyy/engine/IGameLogic.java
@@ -0,0 +1,11 @@
+package com.yyy.engine;
+
+public interface IGameLogic {
+    void init() throws Exception;
+
+    void input(Window window);
+
+    void update(float interval);
+
+    void render(Window window);
+}
---
+++ src/main/java/com/yyy/game/Main.java
@@ -0,0 +1,18 @@
+package com.yyy.game;
+
+import com.yyy.engine.GameEngine;
+import com.yyy.engine.IGameLogic;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            boolean vSync = true;
+            IGameLogic gameLogic = new DummyGame();
+            GameEngine gameEng = new GameEngine("GAME", 600, 480, vSync, gameLogic);
+            gameEng.start();
+        } catch (Exception excp) {
+            excp.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
