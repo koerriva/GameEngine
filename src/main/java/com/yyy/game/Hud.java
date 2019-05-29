cffeb07f60319218b8fd00c89739fc5d84e10ddf
koerriva <rivakoer@gmail.com>
2019-05-29 15:04:12 +0800

    chapter11 HUD 02

--- src/main/java/com/yyy/game/Hud.java
+++ src/main/java/com/yyy/game/Hud.java
@@ -4,6 +4,9 @@ import com.yyy.engine.GameItem;
 import com.yyy.engine.IHud;
 import com.yyy.engine.TextItem;
 import com.yyy.engine.Window;
+import com.yyy.engine.graph.Material;
+import com.yyy.engine.graph.Mesh;
+import com.yyy.engine.graph.OBJLoader;
 import org.joml.Vector4f;
 
 public class Hud implements IHud {
@@ -18,14 +21,31 @@ public class Hud implements IHud {
 
     private final TextItem statusTextItem;
 
+    private final GameItem compassItem;
+
     public Hud(String statusText) throws Exception {
         this.statusTextItem = new TextItem(statusText, FONT_TEXTURE, FONT_COLS, FONT_ROWS);
         this.statusTextItem.getMesh().getMaterial().setAmbientColour(new Vector4f(1, 1, 1, 1));
-        gameItems = new GameItem[]{statusTextItem};
+
+        // Create compass
+        Mesh mesh = OBJLoader.loadMesh("/models/compass.obj");
+        Material material = new Material();
+        material.setAmbientColour(new Vector4f(1, 0, 0, 1));
+        mesh.setMaterial(material);
+        compassItem = new GameItem(mesh);
+        compassItem.setScale(40.0f);
+        // Rotate to transform it to screen coordinates
+        compassItem.setRotation(0f, 0f, 180f);
+
+        gameItems = new GameItem[]{statusTextItem,compassItem};
     }
 
     public void setStatusText(String statusText) {
         this.statusTextItem.setText(statusText);
+    }
+
+    public void rotateCompass(float angle) {
+        this.compassItem.setRotation(0, 0, 180 + angle);
     }
 
     @Override
@@ -35,5 +55,6 @@ public class Hud implements IHud {
 
     public void updateSize(Window window) {
         this.statusTextItem.setPosition(10f, window.getHeight() - 50f, 0);
+        this.compassItem.setPosition(window.getWidth() - 40f, 50f, 0);
     }
 }
